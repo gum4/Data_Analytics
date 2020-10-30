97a0f02ea44572953282f07a35462f4939a7c6a7
NeverThinkOfLWJ <46687992+gum4@users.noreply.github.com>
2020-10-30 15:13:36 -0400

    Update DS_lab4.R

--- DS_lab4.R
+++ DS_lab4.R
@@ -3,8 +3,8 @@ data(Titanic)
 str(Titanic)
 #install.packages("FNN")
 library(FNN)
-x = runif(1000,0,pi)
-e = rnorm(1000,0,0.1)
+x = runif(1000,0,pi)#随机生成服从正态分布的100个随机数
+e = rnorm(1000,0,0.1)#随机生成100个随机数，均值是0，方差是0.1
 y = sin(x)+e
 
 grid2=data.frame(x)
@@ -37,3 +37,46 @@ S=cbind(x,y)
 out.dist=dist(S,method="euclidean") 
 out.hclust=hclust(out.dist,method="complete")
 plot(out.hclust) 
+
+
+library(ISLR)
+library(MASS)
+library(boot)
+set.seed(1)
+
+
+#help("sample")
+train = sample(392,196)
+
+lm.fit <- lm(mpg~horsepower, data = Auto, subset = train)
+
+attach(Auto)
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+lm.fit2 <- lm(mpg~poly(horsepower,2), data = Auto, subset = train) # Quadratic 
+mean((mpg-predict(lm.fit2,Auto))[-train]^2) 
+
+lm.fit3 <- lm(mpg~poly(horsepower,3), data = Auto, subset = train) # Cubic
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+set.seed(2)
+train = sample(392,196)
+lm.fit <- lm(mpg~horsepower, data = Auto, subset = train)
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+
+lm.fit2 <- lm(mpg~poly(horsepower,2), data = Auto, subset = train) # Quadratic 
+mean((mpg-predict(lm.fit2,Auto))[-train]^2) 
+
+lm.fit3 <- lm(mpg~poly(horsepower,3), data = Auto, subset = train) # Cubic
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+#install.packages('randomForest')
+library(randomForest)
+head(Titanic)
+str(Titanic)
+set.seed(100)
+train<-sample(nrow(Titanic),0.7*nrow(Titanic),replace=FALSE)
+train
+model1<-randomForest(Survived ~ .,data=Titanic,importance=TRUE)
+model1
+model2<-randomForest(Survived ~ .,data=Titanic, ntree=500, mtry=6,importance=TRUE)
+model2
